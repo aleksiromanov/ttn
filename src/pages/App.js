072613de3e458f9217b7a69d38befaf2207da0bb
Maxim Zavadskiy <maxim.zavadskiy@medisapiens.fi>
2016-11-26 19:56:05 +0200

    Migrate to bootstrap

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,8 +1,6 @@
 // vim: sw=2:
 
 import React from 'react';
-import Paper from 'material-ui/Paper';
-import RaisedButton from 'material-ui/RaisedButton';
 
 export default class App extends React.Component {
 
@@ -13,60 +11,17 @@ export default class App extends React.Component {
   }
 
   componentWillMount() {
-    this.onChange = this.onChange.bind(this);
-    this.state.content = this.renderFrontpage();
+    this.onClick = this.onClick.bind(this);
   }
 
-  onChange(value) {
-    switch (value) {
-      case 'visits':
-        break;
-      default:
-        alert(`No idea about ${value} ?!`);
-        break;
-    }
-    return true;
+  onClick() {
+    alert('No idea about?!');
   }
-
-  renderVisits = () => ( <Visits />);
-
-  renderFrontpage = () => (
-    <div>
-      <div style={{ margin: 12 }}>
-        <label> Really Want an account? </label>
-        <RaisedButton
-          label={'Sign in'}
-          disableTouchRipple
-          disableFocusRipple
-          secondary
-          onTouchTap={ e => this.onChange('signin') }
-          style={{ marginRight: 12 }}
-        />
-      </div>
-      <div style={{ margin: 12 }}>
-        <label> Otherwise </label>
-        <RaisedButton
-          label={'Sign up'}
-          disableTouchRipple
-          disableFocusRipple
-          primary
-          style={{ marginRight: 12 }}
-          onTouchTap={ e => this.onChange('signup') }
-        />
-      </div>
-    </div>
-  );
-
-  renderDefault = () => (
-    <div>
-      <Paper className="paper" zDepth={0} rounded={false} >
-        {this.state.content}
-      </Paper>
-    </div>
-  );
 
   render () {
-    return this.renderDefault();
+    return <div className='container'>
+      <button className='btn' onClick={this.onClick}> Sign in </button>
+    </div>
   }
 
 }
